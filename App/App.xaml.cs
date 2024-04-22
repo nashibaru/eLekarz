f3fe28aa4c404a809d86fa6a2933328e12f31f36
nashibaru <123249373+nashibaru@users.noreply.github.com>
2024-04-22 17:55:23 +0200

    Add files via upload

---
+++ App/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using eLekarz.Models;
+using eLekarz.Stores;
+using eLekarz.ViewModels;
+using System.Windows;
+
+namespace eLekarz;
+
+public partial class App : Application
+{
+    private readonly AppManager _appManager;
+    private readonly NavigationStore _navigationStore;
+    public App()
+    {
+        _appManager = new AppManager("eLekarz");
+        _navigationStore = new NavigationStore();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _navigationStore.CurrentViewModel = new LoginViewModel(_navigationStore, CreateRegisterViewModel);
+
+        MainWindow = new MainWindow()
+        {
+            DataContext = new MainViewModel(_navigationStore)
+        };
+        MainWindow.Show();
+
+        base.OnStartup(e);
+    }
+
+    private RegisterViewModel CreateRegisterViewModel() => new RegisterViewModel(_appManager, _navigationStore, CreateLoginViewModel);
+
+    private LoginViewModel CreateLoginViewModel() => new LoginViewModel(_navigationStore, CreateRegisterViewModel);
+}
